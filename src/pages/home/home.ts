8d8d3edd7e682be84746deb88caa824c3770c2d2
nikolasbrandao <nikolasbrandao@gmail.com>
2017-03-22 15:28:43 -0300

    add button navigate to AddTaskPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AddTaskPage } from '../add-task/add-task';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,6 +12,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
     
+  }
+
+  createTask(){
+    this.navCtrl.push(AddTaskPage);
   }
 
 }
